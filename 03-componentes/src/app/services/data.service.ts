73afc775ddd6b49f6b0767231e25ad9432dad950
jonathanroberts992 <jonathanroberts992@gmail.com>
2022-04-07 16:39:58 -0300

    menu

--- 03-componentes/src/app/services/data.service.ts
+++ 03-componentes/src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Componente } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,4 +13,7 @@ export class DataService {
     return this.http.get('https://jsonplaceholder.typicode.com/users');
   }
 
+  gettMenuOpts() {
+    return this.http.get<Componente[]>('/assets/data/menu-opts.json');
+  }
 }
