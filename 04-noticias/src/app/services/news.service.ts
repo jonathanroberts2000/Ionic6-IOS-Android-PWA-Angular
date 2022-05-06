33d7641d1006d4267f30b6f2f15a639f2a925bee
jonathanroberts992 <jonathanroberts992@gmail.com>
2022-05-06 17:13:21 -0300

    Nueva aplicacion de noticias - NewsAPI

---
+++ 04-noticias/src/app/services/news.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Article, NewsResponse } from '../interfaces';
+import { map } from 'rxjs/operators';
+
+const apiKey: string = environment.apiKey;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor(private http: HttpClient) { }
+
+  getTopHeadlines(): Observable<Article[]> {
+    return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=business`, {
+      params: { apiKey }
+    }).pipe(
+      //map(data => data.articles)
+      //desestructuracion
+      map(({ articles }) => articles)
+    );
+  }
+}
---
+++ 04-noticias/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: "bc4a3652b042469a9f1f1c1f4b321280"
+};
